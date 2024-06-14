3a0df7e7d9697e8c7b622a2dc7147fc739542943
Ales Zima <ales@cpex.cz>
2024-06-14 16:08:23 +0200

    Added Logo component

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,3 +1,11 @@
+import Logo from '../UI/Logo';
+
 export default function Footer() {
-  return <footer className="bg-neutral-900"></footer>;
+  return (
+    <footer className="bg-neutral-900 py-12">
+      <div className="px-6 max-w-5xl mx-auto">
+        <Logo fillClass="fill-neutral-100" />
+      </div>
+    </footer>
+  );
 }
--- src/components/sections/header/Header.tsx
+++ src/components/sections/header/Header.tsx
@@ -1,4 +1,4 @@
-import logo from '/src/assets/images/logo.svg';
+import Logo from '../../UI/Logo';
 import Navbar from './Navbar';
 
 type HeaderProps = {};
@@ -7,9 +7,7 @@ export default function Header(props: HeaderProps) {
   return (
     <header className="sticky bg-white/80 top-0 z-50 backdrop-blur-[8px]">
       <div className="max-w-5xl mx-auto flex items-center justify-between gap-8 py-8 px-6 md:py-10">
-        <a href="/">
-          <img src={logo.src} alt="Shortly logo" />
-        </a>
+        <Logo />
         <Navbar />
       </div>
     </header>
