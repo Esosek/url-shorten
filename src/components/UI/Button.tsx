0c031506713e44d67e66c55ef875307b047ca0f7
Ales Zima <ales@cpex.cz>
2024-06-17 07:40:11 +0200

    Added errorMessage to input

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,13 +1,19 @@
-import type { ReactNode } from 'react';
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
 
 type ButtonProps = {
   children?: ReactNode;
+  type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
 };
 
-export default function Button({ children, onClick }: ButtonProps) {
+export default function Button({
+  children,
+  type = 'button',
+  onClick,
+}: ButtonProps) {
   return (
     <button
+      type={type}
       onClick={onClick}
       className="bg-cyan-400 text-white py-2 px-6 rounded-md w-full hover:bg-cyan-200 transition-colors md:w-fit"
     >
--- src/components/sections/mid-section/UrlInput.tsx
+++ src/components/sections/mid-section/UrlInput.tsx
@@ -1,22 +1,54 @@
-import { useEffect, useRef, useState } from 'react';
+import {
+  useEffect,
+  useRef,
+  useState,
+  type ChangeEvent,
+  type FormEvent,
+} from 'react';
 import Button from '../../UI/Button';
 import styles from './UrlInput.module.css';
 
 type UrlInputProps = {};
 
 export default function UrlInput(props: UrlInputProps) {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const [wrapperOffset, setWrapperOffset] = useState(0);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [formOffset, setFormOffset] = useState(0);
+  const [userInput, setUserInput] = useState('');
+  const [hasFormError, setHasFormError] = useState(false);
 
   useEffect(() => {
-    const offset = wrapperRef.current?.clientHeight! / 2;
-    setWrapperOffset(offset);
-  }, [wrapperRef]);
+    const offset = formRef.current?.clientHeight! / 2;
+    setFormOffset(offset);
+  }, [formRef]);
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setHasFormError(false);
+    setUserInput(event.target.value);
+  }
+
+  function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (isInputValid()) {
+      console.log('Form submitted');
+    } else {
+      setHasFormError(true);
+    }
+  }
+
+  function isInputValid(): boolean {
+    if (userInput.length > 0) {
+      return true;
+    }
+    setHasFormError(true);
+    return false;
+  }
 
   return (
-    <div
-      ref={wrapperRef}
-      style={{ transform: `translateY(-${wrapperOffset}px)` }}
+    <form
+      ref={formRef}
+      onSubmit={handleFormSubmit}
+      style={{ transform: `translateY(-${formOffset}px)` }}
       className={`${styles.urlInput} grid grid-rows-2 bg-violet gap-4 max-w-5xl mx-auto p-5 rounded-lg md:p-10 md:grid-cols-[1fr_auto] md:grid-rows-1`}
     >
       <label htmlFor="url-input" className="sr-only">
@@ -26,10 +58,19 @@ export default function UrlInput(props: UrlInputProps) {
         type="url"
         name="url-input"
         id="url-input"
+        onChange={handleInputChange}
         placeholder="Shorten a link here..."
-        className="w-full px-5 py-3 rounded-md text-neutral-900"
+        className={`w-full px-5 py-3 rounded-md text-neutral-900 border-2 ${
+          hasFormError && 'border-red'
+        }`}
       />
-      <Button>Shorten It!</Button>
-    </div>
+      {hasFormError && (
+        <p className="absolute bottom-3 left-10 text-sm text-red italic">
+          Please add a link
+        </p>
+      )}
+
+      <Button type="submit">Shorten It!</Button>
+    </form>
   );
 }
