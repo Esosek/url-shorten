bc166b81e9ca0a86a0b567d79a80c9e00880c565
Ales Zima <ales@cpex.cz>
2024-06-18 08:57:20 +0200

    Added UrlHistory list

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -5,6 +5,9 @@ type ButtonProps = {
   type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
   isPrimary?: boolean;
+  bgColor?: string;
+  hoverBgColor?: string;
+  extraClasses?: string;
 };
 
 export default function Button({
@@ -12,13 +15,16 @@ export default function Button({
   type = 'button',
   onClick,
   isPrimary = false,
+  bgColor = 'bg-cyan-400',
+  hoverBgColor = 'hover:bg-cyan-200',
+  extraClasses,
 }: ButtonProps) {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`bg-cyan-400 text-white px-6 
-       rounded-md w-full hover:bg-cyan-200
+      className={`${bgColor} ${hoverBgColor} ${extraClasses} text-white px-6 
+       rounded-md w-full 
        transition-colors md:w-fit
        ${isPrimary ? 'py-3' : 'py-2 text-sm'}`}
     >
--- src/components/sections/mid-section/UrlHistory.tsx
+++ src/components/sections/mid-section/UrlHistory.tsx
@@ -1,16 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styles from './UrlHistory.module.css';
 import Button from '../../UI/Button';
 import URLHistoryContext from '../../context/urlHistory';
 
 export default function UrlHistory() {
   const urlHistory = useContext(URLHistoryContext).urlHistory;
+  const [copiedLink, setCopiedLink] = useState<string>('');
+
+  function handleLinkCopy(link: string) {
+    navigator.clipboard.writeText(link);
+    setCopiedLink(link);
+  }
 
   return (
     <ul className="flex flex-col gap-4 mb-24">
-      {urlHistory.map((url) => {
+      {urlHistory.map((url, index) => {
         return (
-          <li className="relative grid grid-cols-1 grid-rows-3 gap-2 text-sm items-center justify-items-start text-neutral-900 bg-white px-4 md:px-6 pt-2 pb-4 text-left shadow-sm rounded-md md:grid-rows-1 md:gap-6 md:grid-cols-[3fr_1fr_auto] md:text-base md:py-4">
+          <li
+            key={index}
+            className="relative grid grid-cols-1 grid-rows-3 gap-2 text-sm items-center justify-items-start text-neutral-900 bg-white px-4 md:px-6 pt-2 pb-4 text-left shadow-sm rounded-md md:grid-rows-1 md:gap-6 md:grid-cols-[4fr_1fr_auto] md:text-base md:py-4"
+          >
             <p
               className={`${styles.submittedUrl} overflow-ellipsis line-clamp-1 max-w-full`}
             >
@@ -19,7 +28,23 @@ export default function UrlHistory() {
             <a href={url.shortened} className="text-cyan-500">
               {url.shortened}
             </a>
-            <Button>Copy</Button>
+            {copiedLink === url.shortened ? (
+              <Button
+                onClick={() => handleLinkCopy(url.shortened)}
+                bgColor="bg-neutral-700"
+                hoverBgColor="hover:bg-neutral-500"
+                extraClasses="min-w-28"
+              >
+                Copied!
+              </Button>
+            ) : (
+              <Button
+                onClick={() => handleLinkCopy(url.shortened)}
+                extraClasses="min-w-28"
+              >
+                Copy
+              </Button>
+            )}
           </li>
         );
       })}
