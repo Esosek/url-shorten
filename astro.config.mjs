aa63aa4a4a7a792ab218c2bfe24ac3ed4ca9db6a
Ales Zima <ales@cpex.cz>
2024-06-19 12:56:24 +0200

    Reconfigured for Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'hybrid',
-  site: 'https://esosek.github.io',
-  base: 'url-shortener',
+  adapter: netlify(),
 });
